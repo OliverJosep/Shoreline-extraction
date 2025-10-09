cbb1c1c51f51b2feb7d27a0a37f8593488987107
OliverJosep <josep.oliverr@gmail.com>
2025-10-09 15:58:41 +0200

    scripts

--- scripts/train/search_best_patchify.py
+++ scripts/train/search_best_patchify.py
@@ -11,14 +11,14 @@ from src.data_processing.dataset_loader import CoastData
 from src.data_processing.patchify import Patchify
 
 patches = {
-    # "256x256": {
-    #     "patch_size": (256, 256),
-    #     "stride": (128, 128)
-    # },
-    "256x512": {
-        "patch_size": (256, 512),
-        "stride": (128, 256)
+    "256x256": {
+        "patch_size": (256, 256),
+        "stride": (128, 128)
     },
+    # "256x512": {
+    #     "patch_size": (256, 512),
+    #     "stride": (128, 256)
+    # },
     # "256x1024": {
     #     "patch_size": (256, 1024),
     #     "stride": (128, 512)
@@ -67,19 +67,14 @@ def main():
 
         # Load the data split
         print(f"\nLoading data for {key}")
-        patched_data = CoastData(data_path=output_dir)
+        num_classes = 2
+        model = DeepLabV3(num_classes, experiment_name="shoreline_oblique_patchify", use_mlflow=False, pretrained=False)
+        model.load_data(output_dir, CNNFormes, batch_size=2, resize_shape=patches[key]["patch_size"])
 
-        data_split = patched_data.split_data()
-        print(f"\tData loaded")
-    
         # Training
         print(f"\nTraining model for {key}")
-        num_classes = 2
-        model = DeepLabV3(num_classes, experiment_name="shoreline_oblique_patchify", use_mlflow=True, pretrained=False)
-        model.load_data(data_split, CNNFormes, batch_size=2, resize_shape=patches[key]["patch_size"])
-
         description = f"Training DeepLabV3 with patch size {key}, patch_size={patches[key]['patch_size']}, stride={patches[key]['stride']}"
-        model.train(epochs=100, artifact_path=artifact_path, run_description=description)
+        model.train(epochs=1, artifact_path=artifact_path, run_description=description)
         print(f"\tModel trained for {key}")
 
 if __name__ == "__main__":
