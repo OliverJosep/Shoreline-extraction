1aa9b2eceb48fff950558ea3a3eeaf74bc797608
OliverJosep <josep.oliverr@gmail.com>
2026-01-12 14:03:06 +0100

    Update experiment configurations to use 512x512 patch size
    - Modified experiment4_validation.ipynb to change execution counts and update network paths to 512x512 resolution.
    - Updated from_json_to_csv.ipynb to reflect new output paths for 512x512 data.
    - Adjusted kfold.py to set MLFLOW experiment name for 512x512 and updated patch configurations accordingly.

--- scripts/train/kfold.py
+++ scripts/train/kfold.py
@@ -1,40 +1,38 @@
-import sys
 import os
-import matplotlib.pyplot as plt
+import sys
 
 # Add the src directory to the path. TEMPORARY FIX
 sys.path.append(os.getcwd())
 
-from models.deeplab.deeplab import DeepLabV3
-from models.duck_net.duck_net import DuckNet
-from models.unet.unet import UNet
-from models.attention_unet.attention_unet import Attention_UNet
-from models.cnn_model import CNNModel
-
-from src.models.data_management.cnn_formes import CNNFormes
-from src.data_processing.dataset_loader import CoastData
-from src.data_processing.patchify import Patchify
-
+import gc
+import os
 from typing import Type
 
 import pandas as pd
-import os
-
 import torch
-import gc
+
+from models.cnn_model import CNNModel
+from models.deeplab.deeplab import DeepLabV3
+from src.data_processing.dataset_loader import CoastData
+from src.data_processing.patchify import Patchify
+from src.models.data_management.cnn_formes import CNNFormes
 
 USE_MLFLOW = True
-MLFLOW_EXPERIMENT_NAME = "shoreline_kfold"
+MLFLOW_EXPERIMENT_NAME = "shoreline_kfold_512x512"
 
 image_type_paths = {
-    "rectified": {
-        "path": os.path.abspath(os.path.join(os.getcwd(), "data/processed_rectified_3_classes/")),
-        "num_classes": 3
-    },
     "oblique": {
-        "path": os.path.abspath(os.path.join(os.getcwd(), "data/processed_obliques_2_classes/")),
-        "num_classes": 2
-    }
+        "path": os.path.abspath(
+            os.path.join(os.getcwd(), "data/processed_obliques_2_classes/")
+        ),
+        "num_classes": 2,
+    },
+    "rectified": {
+        "path": os.path.abspath(
+            os.path.join(os.getcwd(), "data/processed_rectified_3_classes/")
+        ),
+        "num_classes": 3,
+    },
 }
 
 networks: dict[str, Type[CNNModel]] = {
@@ -42,54 +40,59 @@ networks: dict[str, Type[CNNModel]] = {
 }
 
 patches = {
-    "256x1024": {
-        "patch_size": (256, 1024),
-        "stride": (128, 512)
-    },
+    "512x512": {"patch_size": (512, 512), "stride": (256, 256)},
 }
+
 
 def generate_patches(data, output_dir, patch_size, stride):
     patchify = Patchify(patch_size, stride)
-    patchify.extract_patches_and_save(data, output_dir, skip_no_shoreline=True, binary_class=True, p_keep_negative=1, padding_mode='reflect')
+    patchify.extract_patches_and_save(
+        data,
+        output_dir,
+        skip_no_shoreline=True,
+        binary_class=True,
+        p_keep_negative=1,
+        padding_mode="reflect",
+    )
 
 
-def export_folds_summary(data_splits, output_dir='.'):
+def export_folds_summary(data_splits, output_dir="."):
     file_roles = {}
 
     for fold_name in data_splits.keys():
-        for split_type in ['validation', 'train', 'test']:
-            if data_splits[fold_name][split_type].get('images'):
-
-                file_paths = data_splits[fold_name][split_type]['images']
+        for split_type in ["validation", "train", "test"]:
+            if data_splits[fold_name][split_type].get("images"):
+                file_paths = data_splits[fold_name][split_type]["images"]
 
                 for file_path in file_paths:
                     filename = os.path.basename(file_path)
 
                     if filename not in file_roles:
                         file_roles[filename] = {}
-                    
+
                     file_roles[filename][fold_name] = split_type
 
-    df = pd.DataFrame.from_dict(file_roles, orient='index')
+    df = pd.DataFrame.from_dict(file_roles, orient="index")
 
     df = df.reindex(columns=sorted(data_splits.keys()))
-    df = df.reset_index().rename(columns={'index': 'filename'})
+    df = df.reset_index().rename(columns={"index": "filename"})
 
-    output_filename = os.path.join(output_dir, 'folds_summary.csv')
+    output_filename = os.path.join(output_dir, "folds_summary.csv")
     df.to_csv(output_filename, index=False)
 
     print(df.head())
-    
+
 
 def main():
-
     for data_type in image_type_paths:
-        print(f"\n{'#'*30}\nProcessing {data_type} images\n{'#'*30}")
+        print(f"\n{'#' * 30}\nProcessing {data_type} images\n{'#' * 30}")
         data_path = image_type_paths[data_type]["path"]
         num_classes = image_type_paths[data_type]["num_classes"]
 
         # Load data
-        artifact_path = os.path.abspath(os.path.join(os.getcwd(), "artifacts/"))
+        artifact_path = os.path.abspath(
+            os.path.join(os.getcwd(), "artifacts/")
+        )
 
         data = CoastData(data_path)
 
@@ -97,54 +100,91 @@ def main():
         data_splits = data.get_kfold_splits(k, test_size=0.1)
         print(f"Generated {k}-fold splits with test size 0.1")
         for fold in data_splits.keys():
-            print(f"\n{'='*20}\nStarting {fold}\n{'='*20}")
-            print(f"Train size: {len(data_splits[fold]['train']['images'])}, Val size: {len(data_splits[fold]['validation']['images'])}, Test size: {len(data_splits[fold]['test']['images'])}")
+            print(f"\n{'=' * 20}\nStarting {fold}\n{'=' * 20}")
+            print(
+                f"Train size: {len(data_splits[fold]['train']['images'])}, Val size: {len(data_splits[fold]['validation']['images'])}, Test size: {len(data_splits[fold]['test']['images'])}"
+            )
             data = data_splits[fold]
 
             # Generate patches for each configuration if not already done
             for key in patches.keys():
                 print(f"\nGenerating patches for {key}")
 
-                output_dir = os.path.abspath(os.path.join(os.getcwd(), f"data/{k}fold/{data_type}/{fold}_patchify_{num_classes}_classes_{data_type}_{key}/"))
+                output_dir = os.path.abspath(
+                    os.path.join(
+                        os.getcwd(),
+                        f"data/{k}fold/{data_type}/{fold}_patchify_{num_classes}_classes_{data_type}_{key}/",
+                    )
+                )
                 if not os.path.exists(output_dir):
                     print(f"\tCreating directory {output_dir}...")
                     os.makedirs(output_dir)
-                    generate_patches(data, output_dir, patches[key]["patch_size"], patches[key]["stride"])
+                    generate_patches(
+                        data,
+                        output_dir,
+                        patches[key]["patch_size"],
+                        patches[key]["stride"],
+                    )
                     print(f"\tDone generating patches for {key}")
                 else:
-                    print(f"\tDirectory {output_dir} already exists. Skipping...")
+                    print(
+                        f"\tDirectory {output_dir} already exists. Skipping..."
+                    )
 
-        output_dir = os.path.abspath(os.path.join(os.getcwd(), f"data/{k}fold/{data_type}/"))
+        output_dir = os.path.abspath(
+            os.path.join(os.getcwd(), f"data/{k}fold/{data_type}/")
+        )
         export_folds_summary(data_splits, output_dir=output_dir)
 
         for network in networks:
-            print(f"\n{'='*30}\nStarting training for {network}\n{'='*30}")
+            print(f"\n{'=' * 30}\nStarting training for {network}\n{'=' * 30}")
             for fold in data_splits.keys():
-                print(f"\n{'-'*20}\nTraining fold: {fold}\n{'-'*20}")
-        
+                print(f"\n{'-' * 20}\nTraining fold: {fold}\n{'-' * 20}")
+
                 for key in patches.keys():
-                    output_dir = os.path.abspath(os.path.join(os.getcwd(), f"data/{k}fold/{data_type}/{fold}_patchify_{num_classes}_classes_{data_type}_{key}/"))
+                    output_dir = os.path.abspath(
+                        os.path.join(
+                            os.getcwd(),
+                            f"data/{k}fold/{data_type}/{fold}_patchify_{num_classes}_classes_{data_type}_{key}/",
+                        )
+                    )
                     # Load the data split
                     print(f"\nLoading data for {key} - fold {fold}")
                     print(f"\tFrom directory: {output_dir}")
 
+                    model = networks[network](
+                        num_classes,
+                        experiment_name=MLFLOW_EXPERIMENT_NAME,
+                        use_mlflow=USE_MLFLOW,
+                    )
 
-                    model = networks[network](num_classes, experiment_name=MLFLOW_EXPERIMENT_NAME, use_mlflow=USE_MLFLOW)
-
-                    model.load_data(output_dir, CNNFormes, batch_size=24, resize_shape=patches[key]["patch_size"], drop_last=True)
+                    model.load_data(
+                        output_dir,
+                        CNNFormes,
+                        batch_size=24,
+                        resize_shape=patches[key]["patch_size"],
+                        drop_last=True,
+                    )
 
                     # Training
                     print(f"\nTraining model for {key} - fold {fold}")
                     run_name = f"{data_type}_{fold}_{network}_{key}"
                     description = f"Dataset type: {data_type}, Training {network} with patch size {key}, patch_size={patches[key]['patch_size']}, stride={patches[key]['stride']}, fold: {fold}"
                     early_stopping = 10
-                    model.train(epochs=100, artifact_path=artifact_path, run_name=run_name, run_description=description, early_stopping=early_stopping)
+                    model.train(
+                        epochs=100,
+                        artifact_path=artifact_path,
+                        run_name=run_name,
+                        run_description=description,
+                        early_stopping=early_stopping,
+                    )
                     print(f"\tModel trained for {key} - fold {fold}")
 
                     # Clear memory
                     del model
                     gc.collect()
                     torch.cuda.empty_cache()
+
 
 if __name__ == "__main__":
     main()
